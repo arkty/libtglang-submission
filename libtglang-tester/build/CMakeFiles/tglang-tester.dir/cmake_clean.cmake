8acd48c422c2253b00958c61eef0cde8894024d6
Andrey Khitryy <andrey.khitryy@gmail.com>
2023-10-15 15:04:04 +0300

    Add tester & model

---
+++ libtglang-tester/build/CMakeFiles/tglang-tester.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tglang-tester.dir/tglang-tester.c.o"
+  "tglang-tester.pdb"
+  "tglang-tester"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tglang-tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
